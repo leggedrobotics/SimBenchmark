e17b7cee3bef12988bb20b8f40a18700b6b8ca59
donghokang <east0822@gmail.com>
2018-02-19 15:34:04 +0900

    - mujoco model directory was added to res directory - single body object get position and get orientation were implemented

--- include/mujocoSim/object/SingleBodyObject.cpp
+++ include/mujocoSim/object/SingleBodyObject.cpp
@@ -10,19 +10,29 @@ namespace object {
 SingleBodyObject::SingleBodyObject(mjData *data, int objectID) : worldData_(data), objectID_(objectID) {}
 
 const Eigen::Map<Eigen::Matrix<double, 4, 1>> SingleBodyObject::getQuaternion() {
-  return Eigen::Map<Eigen::Matrix<double, 4, 1>>(nullptr);
+  mjtNum *quaternion = (worldData_->xquat + 4 * objectID_);
+  quatTemp_ = {quaternion[0], quaternion[1], quaternion[2], quaternion[3]};
+  return quatTemp_.e();
 }
 
 void SingleBodyObject::getQuaternion(rai_sim::Vec<4> &quat) {
-
+  mjtNum *quaternion = (worldData_->xquat + 4 * objectID_);
+  quat = {quaternion[0], quaternion[1], quaternion[2], quaternion[3]};
 }
 
 const Eigen::Map<Eigen::Matrix<double, 3, 3> > SingleBodyObject::getRotationMatrix() {
-  return Eigen::Map<Eigen::Matrix<double, 3, 3>>(nullptr);
+  mjtNum *rotMat = (worldData_->xmat + 9 * objectID_);
+  rotMatTemp_.e() << rotMat[0], rotMat[1], rotMat[2],
+      rotMat[3], rotMat[4], rotMat[5],
+      rotMat[6], rotMat[7], rotMat[8];
+  return rotMatTemp_.e();
 }
 
 void SingleBodyObject::getRotationMatrix(rai_sim::Mat<3, 3> &rotation) {
-
+  mjtNum *rotMat = (worldData_->xmat + 9 * objectID_);
+  rotation.e() << rotMat[0], rotMat[1], rotMat[2],
+      rotMat[3], rotMat[4], rotMat[5],
+      rotMat[6], rotMat[7], rotMat[8];
 }
 
 const Eigen::Map<Eigen::Matrix<double, 3, 1> > SingleBodyObject::getPosition() {
@@ -32,21 +42,25 @@ const Eigen::Map<Eigen::Matrix<double, 3, 1> > SingleBodyObject::getPosition() {
 }
 
 const Eigen::Map<Eigen::Matrix<double, 3, 1> > SingleBodyObject::getComPosition() {
-  mjtNum *pos = (worldData_->xpos + 3 * objectID_);
+  mjtNum *pos = (worldData_->xipos + 3 * objectID_);
   posTemp_ = {pos[0], pos[1], pos[2]};
-  RAIWARN('check if COM = body origin!');
   return posTemp_.e();
 }
 
 const Eigen::Map<Eigen::Matrix<double, 3, 1> > SingleBodyObject::getLinearVelocity() {
-  return Eigen::Map<Eigen::Matrix<double, 3, 1>>(nullptr);
+  mjtNum *linVel = (worldData_->cvel + 6 * objectID_ + 3);
+  linVelTemp_ = {linVel[0], linVel[1], linVel[2]};
+  return linVelTemp_.e();
 }
 
 const Eigen::Map<Eigen::Matrix<double, 3, 1> > SingleBodyObject::getAngularVelocity() {
-  return Eigen::Map<Eigen::Matrix<double, 3, 1>>(nullptr);
+  mjtNum *angVel = (worldData_->cvel + 6 * objectID_);
+  angVelTemp_ = {angVel[0], angVel[1], angVel[2]};
+  return angVelTemp_.e();
 }
 void SingleBodyObject::getPosition_W(rai_sim::Vec<3> &pos_w) {
-
+  mjtNum *pos = (worldData_->xpos + 3 * objectID_);
+  pos_w = {pos[0], pos[1], pos[2]};
 }
 
 } // object
