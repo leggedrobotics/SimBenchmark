7c225ba8fd9852346ce7d6b7650ac782b40eb654
donghokang <east0822@gmail.com>
2018-04-15 10:24:11 +0200

    - odeSim  - created articulatedsystem class

---
+++ sim/odeSim/src/object/OdeArticulatedSystem.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kangd on 15.04.18.
+//
+
+#include "OdeArticulatedSystem.hpp"
+
+namespace ode_sim {
+namespace object {
+
+OdeArticulatedSystem::OdeArticulatedSystem(std::string urdf) {
+
+}
+
+} // object
+} // ode_sim
---
+++ sim/odeSim/src/object/OdeArticulatedSystem.hpp
@@ -0,0 +1,23 @@
+//
+// Created by kangd on 15.04.18.
+//
+
+#ifndef BENCHMARK_ODEARTICULATEDSYSTEM_HPP
+#define BENCHMARK_ODEARTICULATEDSYSTEM_HPP
+
+#include <string>
+
+namespace ode_sim {
+namespace object {
+
+class OdeArticulatedSystem {
+
+ public:
+  OdeArticulatedSystem(std::string urdf);
+
+};
+
+} // object
+} // ode_sim
+
+#endif //BENCHMARK_ODEARTICULATEDSYSTEM_HPP
