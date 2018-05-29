bc9eaf4ab3f7ff93ec4d899ce84ec502d6248018
donghokang <east0822@gmail.com>
2018-05-29 16:26:12 +0200

    - solverResidualThreshold setter bug fixed

--- sim/bulletMultibodySim/src/api/b3RobotSimulatorClientAPI_NoGUI.cpp
+++ sim/bulletMultibodySim/src/api/b3RobotSimulatorClientAPI_NoGUI.cpp
@@ -1697,7 +1697,7 @@ bool b3RobotSimulatorClientAPI_NoGUI::setPhysicsEngineParameter(struct b3RobotSi
 		b3PhysicsParamSetDefaultFrictionERP(command, args.m_frictionERP);
 	}
 
-	if (args.m_restitutionVelocityThreshold >= 0)
+	if (args.m_solverResidualThreshold >= 0)
 	{
 		b3PhysicsParamSetSolverResidualThreshold(command, args.m_solverResidualThreshold);
 	}
