6b2162ce1f8c9e5ba1faa0312578991646d306c9
east0822@gmail.com <donghokang>
2018-12-08 13:02:07 +0100

    bullet works now.

--- sim/bulletSim/demo/AnymalDemo.cpp
+++ sim/bulletSim/demo/AnymalDemo.cpp
@@ -7,66 +7,64 @@
 #include "BtMbSim.hpp"
 #include "raiCommon/utils/StopWatch.hpp"
 
+#define URDFPATH ROOTPATH "/res/benchmark/ANYmal-PD-benchmark/bullet/robot.urdf"
 //#define VIDEO_SAVE_MODE
 
 namespace po = boost::program_options;
 
 int main(int argc, const char* argv[]) {
 
-  std::string urdfPath(__FILE__);
-  while (urdfPath.back() != '/')
-    urdfPath.erase(urdfPath.size() - 1, 1);
-  urdfPath += "../../../res/ANYmal-nomesh/robot.urdf";
+    std::string urdfPath = std::string(URDFPATH);
 
-  bullet_mb_sim::BtMbSim sim(800, 600, 0.5, benchmark::NO_BACKGROUND);
+    bullet_mb_sim::BtMbSim sim(800, 600, 0.5, benchmark::NO_BACKGROUND);
 
-  auto checkerboard = sim.addCheckerboard(2, 100, 100, 0.1, bo::BOX_SHAPE, 1, -1, bo::GRID);
-  checkerboard->setFrictionCoefficient(0.8);
+    auto checkerboard = sim.addCheckerboard(2, 100, 100, 0.1, bo::BOX_SHAPE, 1, -1, bo::GRID);
+    checkerboard->setFrictionCoefficient(0.8);
 
-  auto anymal = sim.addArticulatedSystem(urdfPath, bullet_mb_sim::object::URDF);
-  anymal->setGeneralizedCoordinate(
-      {0, 0, 0.54,
-       1.0, 0.0, 0.0, 0.0,
-       0.03, 0.4, -0.8,
-       -0.03, 0.4, -0.8,
-       0.03, -0.4, 0.8,
-       -0.03, -0.4, 0.8});
+    auto anymal = sim.addArticulatedSystem(urdfPath, bullet_mb_sim::object::URDF);
+    anymal->setGeneralizedCoordinate(
+            {0, 0, 0.54,
+             1.0, 0.0, 0.0, 0.0,
+             0.03, 0.4, -0.8,
+             -0.03, 0.4, -0.8,
+             0.03, -0.4, 0.8,
+             -0.03, -0.4, 0.8});
 //  anymal->setGeneralizedVelocity(Eigen::VectorXd::Zero(anymal->getDOF()));
-  anymal->setGeneralizedForce(Eigen::VectorXd::Zero(anymal->getDOF()));
+    anymal->setGeneralizedForce(Eigen::VectorXd::Zero(anymal->getDOF()));
 
-  sim.setGravity({0, 0, -9.81});
-  sim.setERP(0.05, 0.01, 0.0);
-  sim.setTimeStep(0.005);
+    sim.setGravity({0, 0, -9.81});
+    sim.setERP(0.05, 0.01, 0.0);
+    sim.setTimeStep(0.005);
 
-  Eigen::VectorXd jointNominalConfig(19);
-  Eigen::VectorXd jointState(18), jointVel(18), jointForce(18);
-  const double kp = 40.0, kd = 1.0;
+    Eigen::VectorXd jointNominalConfig(19);
+    Eigen::VectorXd jointState(18), jointVel(18), jointForce(18);
+    const double kp = 40.0, kd = 1.0;
 
-  jointNominalConfig << 0, 0, 0,
-      1.0, 0, 0, 0,
-      0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8;
+    jointNominalConfig << 0, 0, 0,
+            1.0, 0, 0, 0,
+            0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8;
 
-  sim.cameraFollowObject(checkerboard, {1.0, 1.0, 1.0});
+    sim.cameraFollowObject(checkerboard, {1.0, 1.0, 1.0});
 
 #if defined(VIDEO_SAVE_MODE)
-  sim.startRecordingVideo("/tmp", "btAnymal");
+    sim.startRecordingVideo("/tmp", "btAnymal");
   for(int i = 0; i < 2000 && sim.visualizerLoop(0.005, 1.0); i++) {
 #else
-  while(sim.visualizerLoop(0.005, 1.0)) {
+    while(sim.visualizerLoop(0.005, 1.0)) {
 #endif
-    jointState = anymal->getGeneralizedCoordinate();
-    jointVel = anymal->getGeneralizedVelocity();
-    jointForce = anymal->getGeneralizedForce();
+        jointState = anymal->getGeneralizedCoordinate();
+        jointVel = anymal->getGeneralizedVelocity();
+//    jointForce = anymal->getGeneralizedForce();
 
-    jointForce = kp * (jointNominalConfig - jointState).tail(18) - kd * jointVel;
-    jointForce.head(6).setZero();
-    anymal->setGeneralizedForce(jointForce);
-    sim.integrate();
-  }
+        jointForce = kp * (jointNominalConfig - jointState).tail(18) - kd * jointVel;
+        jointForce.head(6).setZero();
+        anymal->setGeneralizedForce(jointForce);
+        sim.integrate();
+    }
 
 #if defined(VIDEO_SAVE_MODE)
-  sim.stopRecordingVideo();
+    sim.stopRecordingVideo();
 #endif
 
-  return 0;
+    return 0;
 }
--- sim/bulletSim/demo/AnymalEnergyDemo.cpp
+++ sim/bulletSim/demo/AnymalEnergyDemo.cpp
@@ -8,6 +8,7 @@
 #include "raiCommon/utils/StopWatch.hpp"
 
 //#define VIDEO_SAVE_MODE
+#define URDFPATH ROOTPATH "/res/benchmark/ANYmal-energy-benchmark/bullet/robot.urdf"
 
 namespace po = boost::program_options;
 std::vector<double> kenergy;
@@ -16,24 +17,21 @@ void showplot(double);
 
 int main(int argc, const char* argv[]) {
 
-  std::string urdfPath(__FILE__);
-  while (urdfPath.back() != '/')
-    urdfPath.erase(urdfPath.size() - 1, 1);
-  urdfPath += "../../../res/bullet/ANYmal-energy/robot.urdf";
+    std::string urdfPath = std::string(URDFPATH);
 
-  bullet_mb_sim::BtMbSim sim(800, 600, 0.5, benchmark::NO_BACKGROUND);
+    bullet_mb_sim::BtMbSim sim(800, 600, 0.5, benchmark::NO_BACKGROUND);
 
-  auto checkerboard = sim.addCheckerboard(2, 100, 100, 0.1, bo::PLANE_SHAPE, 1, -1, bo::GRID);
-  checkerboard->setFrictionCoefficient(0.8);
+    auto checkerboard = sim.addCheckerboard(2, 100, 100, 0.1, bo::PLANE_SHAPE, 1, -1, bo::GRID);
+    checkerboard->setFrictionCoefficient(0.8);
 
-  auto anymal = sim.addArticulatedSystem(urdfPath, bullet_mb_sim::object::URDF, false);
-  anymal->setGeneralizedCoordinate(
-      {0, 0, 10,
-       1.0, 0.0, 0.0, 0.0,
-       0.03, 0.4, -0.8,
-       -0.03, 0.4, -0.8,
-       0.03, -0.4, 0.8,
-       -0.03, -0.4, 0.8});
+    auto anymal = sim.addArticulatedSystem(urdfPath, bullet_mb_sim::object::URDF, false);
+    anymal->setGeneralizedCoordinate(
+            {0, 0, 10,
+             1.0, 0.0, 0.0, 0.0,
+             0.03, 0.4, -0.8,
+             -0.03, 0.4, -0.8,
+             0.03, -0.4, 0.8,
+             -0.03, -0.4, 0.8});
 //  anymal->setGeneralizedCoordinate(
 //      {0, 0, 10,
 //       1.0, 0.0, 0.0, 0.0,
@@ -41,62 +39,62 @@ int main(int argc, const char* argv[]) {
 //       0, 0, 0,
 //       0, 0, 0,
 //       0, 0, 0});
-  anymal->setGeneralizedVelocity(
-      {0, 0, 0,
-       0.2, 1.0, 0.5,
-       0.0, 0.0, 0.0,
-       0.0, 0.0, 0.0,
-       0.0, 0.0, 0.0,
-       0.0, 0.0, 0.0});
-  RAIINFO(anymal->getGeneralizedVelocity())
+    anymal->setGeneralizedVelocity(
+            {0, 0, 0,
+             0.2, 1.0, 0.5,
+             0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0,
+             0.0, 0.0, 0.0});
+    RAIINFO(anymal->getGeneralizedVelocity())
 
-  double g = 0;
-  double dt = 0.005;
-  sim.setGravity({0, 0, g});
-  sim.setTimeStep(dt);
+    double g = 0;
+    double dt = 0.005;
+    sim.setGravity({0, 0, g});
+    sim.setTimeStep(dt);
 
-  sim.cameraFollowObject(checkerboard, {15.0, 0.0, 15.0});
+    sim.cameraFollowObject(checkerboard, {15.0, 0.0, 15.0});
 
-  double E0 = anymal->getEnergy({0, 0, g});
-  for(int i = 0; i < int(5.0/dt) && sim.visualizerLoop(dt, 1.0); i++) {
-    kenergy.push_back(anymal->getEnergy({0, 0, g}));
-    sim.integrate();
-  }
+    double E0 = anymal->getEnergy({0, 0, g});
+    for(int i = 0; i < int(5.0/dt) && sim.visualizerLoop(dt, 1.0); i++) {
+        kenergy.push_back(anymal->getEnergy({0, 0, g}));
+        sim.integrate();
+    }
 
-  RAIINFO("initial E = " << E0)
-  showplot(E0);
-  return 0;
+    RAIINFO("initial E = " << E0)
+    showplot(E0);
+    return 0;
 }
 
 void showplot(double E0) {
 
-  int n = kenergy.size();
-  Eigen::MatrixXd tdata(n, 1);
-  Eigen::MatrixXd Edata(n, 1);
-  Eigen::MatrixXd edata(n, 1);
+    int n = kenergy.size();
+    Eigen::MatrixXd tdata(n, 1);
+    Eigen::MatrixXd Edata(n, 1);
+    Eigen::MatrixXd edata(n, 1);
 
-  for(int i = 0; i < n; i++) {
-    tdata(i, 0) = i;
-    Edata(i, 0) = kenergy[i];
-    edata(i, 0) = E0 - kenergy[i];
-  }
+    for(int i = 0; i < n; i++) {
+        tdata(i, 0) = i;
+        Edata(i, 0) = kenergy[i];
+        edata(i, 0) = E0 - kenergy[i];
+    }
 
-  rai::Utils::Graph::FigProp2D figure1properties("step", "Energy", "Energy");
-  rai::Utils::graph->figure(1, figure1properties);
-  rai::Utils::graph->appendData(1,
-                                tdata.data(),
-                                Edata.data(),
-                                n,
-                                "E");
-  rai::Utils::graph->drawFigure(1);
+    rai::Utils::Graph::FigProp2D figure1properties("step", "Energy", "Energy");
+    rai::Utils::graph->figure(1, figure1properties);
+    rai::Utils::graph->appendData(1,
+                                  tdata.data(),
+                                  Edata.data(),
+                                  n,
+                                  "E");
+    rai::Utils::graph->drawFigure(1);
 
-  rai::Utils::Graph::FigProp2D figure2properties("step", "Energy error", "Energy error");
-  rai::Utils::graph->figure(2, figure2properties);
-  rai::Utils::graph->appendData(2,
-                                tdata.data(),
-                                edata.data(),
-                                n,
-                                "error");
-  rai::Utils::graph->drawFigure(2);
-  rai::Utils::graph->waitForEnter();
+    rai::Utils::Graph::FigProp2D figure2properties("step", "Energy error", "Energy error");
+    rai::Utils::graph->figure(2, figure2properties);
+    rai::Utils::graph->appendData(2,
+                                  tdata.data(),
+                                  edata.data(),
+                                  n,
+                                  "error");
+    rai::Utils::graph->drawFigure(2);
+    rai::Utils::graph->waitForEnter();
 }
